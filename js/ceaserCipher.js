79b545223e31ea445944e2a360c2dfc31acb1805
deepspacediver <luka.koscik@gmail.com>
2022-11-20 18:29:57 +0100

    ceaser tests

--- js/ceaserCipher.js
+++ js/ceaserCipher.js
@@ -1,4 +1,25 @@
-const ceaserCipher = () => {};
+const shiftUTF16 = (character, shift) => {
+  let shiftedChar = String.fromCharCode(character.charCodeAt(0) + shift);
+  let shiftedCode = shiftedChar.charCodeAt(0);
 
+  // Checks for lower cased characters wrapping over "z"
+  if (shiftedCode > 122)
+    shiftedChar = String.fromCharCode(96 + (shiftedCode - 122));
 
-export default ceaserCipher
+  return shiftedChar;
+};
+
+const ceaserCipher = (string, shift) => {
+  const shiftedArr = [];
+  let stringToArray = string.split("");
+  stringToArray.forEach((el) => {
+    if (el === " " || el === "," || el === ".") shiftedArr.push(el);
+    else {
+      let newChar = shiftUTF16(el, shift);
+      shiftedArr.push(newChar);
+    }
+  });
+  return shiftedArr.join("");
+};
+
+export default ceaserCipher;
--- js/tests/ceaserCipher.test.js
+++ js/tests/ceaserCipher.test.js
@@ -1,1 +1,23 @@
 import ceaserCipher from "../ceaserCipher";
+
+test("Shifts lower case letters", () => {
+  expect(ceaserCipher("a", 1)).toBe("b"),
+    expect(ceaserCipher("b", 1)).toBe("c");
+  expect(ceaserCipher("a", 3)).toBe("d");
+  expect(ceaserCipher("z", 1)).toBe("a");
+  expect(ceaserCipher("y", 25)).toBe("x");
+});
+
+test("Shifts lower-cased strings", () => {
+  expect(ceaserCipher("abba", 1)).toBe("bccb");
+  expect(ceaserCipher("zorro", 1)).toBe("apssp");
+});
+
+test("Shifts lower-cased sentence", () => {
+  expect(ceaserCipher("hello this is ceaser z", 1)).toBe(
+    "ifmmp uijt jt dfbtfs a"
+  );
+  expect(ceaserCipher("hello this is ceaser z", 25)).toBe(
+    "gdkkn sghr hr bdzrdq y"
+  );
+});
